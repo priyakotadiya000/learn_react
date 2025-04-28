62f62c6d52f3857531c7084376e4878f123118f5
jk-0 <j.kotadiya.96@gmail.com>
2025-04-28 18:17:23 +0530

    delete,update,crate,list

--- 5_learn_api_crud_project/src/componets/Header.jsx
+++ 5_learn_api_crud_project/src/componets/Header.jsx
@@ -1,7 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate ,useLocation} from "react-router-dom";
+
 
 const Header = () => {
     const navigate = useNavigate();
+    const location = useLocation(); // Current path milta hai
+
 
     const handleClick = () => {
       navigate("/about");
@@ -12,13 +15,24 @@ const Header = () => {
       localStorage.clear();
  
     }
-    
+    const handleTagButton = () => {
+      alert("Tag Button Clicked!");
+      // Ya navigate kar sakte ho kahin bhi:
+      // navigate('/tag-create')
+  };
+
 
   return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container-fluid justify-content-end">
+              
               <button onClick={handleClick}>About</button>
               <button onClick={handleClickby}>Logout</button>
+              {location.pathname.startsWith("/project/") && (
+          <button onClick={handleTagButton} className="btn btn-success">
+            + Tag Name
+          </button>
+        )}
         </div>
       </nav>
   );
